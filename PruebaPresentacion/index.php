adf621ad87df721c8a9f7c05d09413c5cdc81d79
JMR <derosasjm@gmail.com>
2023-12-12 14:27:21 -0300

    12/12/23 Subiendo WebHook - Prueba 1

--- PruebaPresentacion/index.php
+++ PruebaPresentacion/index.php
@@ -1,1 +1,25 @@
-<h1>BOT Telegram</h1>
+<?php
+
+
+require_once 'include/vendor/autoload.php';
+
+use TelegramBot\Api\BotApi;
+
+//Configurar el token de acceso al Bot
+$Telegram = new BotAp('6918082719:AAGOKaGsZVg1-YK7KPhICKj-kbiK4NLGtes');
+
+//Obtiene la actualizacion del WebHook
+$Update = json_decode(file_get_contents('php:\\input'));
+
+//Verificar si se recibió un mensaje de texto
+if(isset($Update->message->text)){
+    //Se guarda en una variable el id del chat y el mensaje
+    $ChatId = $Update->message->chat->id;
+    $ChatMensaje = $Update->message->text;
+
+
+    $telegram->sendMessage($ChatId,"Lo que escribió el usuario es: ".$ChatMensaje);
+    
+}
+
+?>
